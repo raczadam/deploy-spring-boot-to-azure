2c6f8868cd02f6465bab22104783e6cf611d9753
racza <racza@DESKTOP-5QALNLJ>
2023-03-03 21:23:02 +0100

    Add:
    - gitignores
    - lombok to maven
    - some logging

--- 01-hello-world-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ 01-hello-world-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -4,11 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
+		log.info("helloWorld invoked");
 		return "Hello World";
 	}
 
@@ -16,12 +20,15 @@ public class HelloWorldController {
 	public HelloWorldBean helloWorldBean() {
 		// throw new RuntimeException("Some Error has Happened! Contact Support at
 		// ***-***");
+		log.info("helloWorldBean invoked");
 		return new HelloWorldBean("Hello World");
 	}
 
 	/// hello-world/path-variable/in28minutes
 	@GetMapping(path = "/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		System.out.println("simple console print");
+		log.info("helloWorldPathVariable invoked");
 		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 }
--- 01-hello-world-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ 01-hello-world-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -3,10 +3,14 @@ package com.in28minutes.rest.webservices.restfulwebservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class RestfulWebServicesApplication {
 
 	public static void main(String[] args) {
+		log.info("Application start");
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
 }
